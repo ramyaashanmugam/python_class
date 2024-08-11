b0c2ae8b87cc6f09b459d28c4e0f853dea23e8d7
unknown <ramyashanmugam1507@gmail.com>
2024-08-12 00:16:56 +0530

    conditional and loop prgms

--- conditinal_statements.py
+++ conditinal_statements.py
@@ -1,4 +1,4 @@
-# if statement
+'''# if statement
 x=5
 if x == 5:
     print(x)
@@ -16,14 +16,14 @@ if val %2==0:
     print("gevin value is a even number")
 else:
     print("given value is odd number") 
-
+'''
 ''' if,elif,else prerequest
 1. take input from user stutent name
 2. need to check with student name == ramya or shreena
 3. if student name satisfy print( student gelongs to IT depatment)
 4. else print student doesnot belongs to IT
 '''
-
+'''
 student=input("enter student name:")
 if student=='ramya':
     print("student belongs to IT depatment")
@@ -44,7 +44,7 @@ elif student.lower()=='shreena':
     print("student belongs to IT depatment")
 else:
     print("student does not belongs to IT depatment")
-
+'''
 
 '''
 create 5 student name list1 with ramya,shreena,mamo,vino,ram
@@ -54,7 +54,7 @@ student present in list1 print student belongs to class A or ckck with list2 pri
 otherwise print student does not belongs to A or B section need to ckeck with different section
 
 '''
-
+'''
 list1=["ramya","shreena","mano","vino","ram"]
 list2=["raju","prasanna","radha","krish",'adidev']
 student_name=input("enter the student name with lowercase:")
@@ -64,4 +64,100 @@ elif student_name in list2:
     print("student belongs to class B")
 else:
     print("student does not belongs to A or B section need to ckeck with different section")
+'''
+'''
+units = float(input("Enter the total units of electricity consumed: "))
+
+# Initialize the bill amount
+bill_amount = 0
+
+# Calculate the bill based on the given criteria
+if units > 300:
+    bill_amount += (units - 300) * 5
+    units = 300
+if units > 100:
+    bill_amount += (units - 100) * 2
+
+# Display the calculated bill amount
+print(f"The total electric bill is: {bill_amount:.2f} Rs")
+'''
+
+numbers = [10, 40, 3, 40]
+threshold = 5
+
+if numbers[0] >= threshold:
+    if numbers[1] >= threshold:
+        if numbers[2] >= threshold:
+            if numbers[3] >= threshold:
+                print("all elements in a list are greater than threshold value")
+            else:
+                print("all elements in a list is not greater than threshold value")
+        else:
+            print("all elements in a list is not greater than threshold value")
+    else:
+         print("all elements in a list is not greater than threshold value")
+else:
+    print("all elements in a list is not greater than threshold value")
+
+#if any of the element in a lis is even or not
+l1=[5,9,8,9]
+if l1[0] %2 !=0:
+    if l1[1] %2 !=0:
+     if l1[2] %2 !=0:
+      if l1[3] %2 !=0:
+       print("all elenment in a list is odd")
+      else:
+         print("even")
+     else:
+         print("even")
+    else:
+        print("even")
+else:
+    print("even")
+
+val=5
+out=''
+for i in range(val):
+   out= out+str(i+1)
+print(out)
+
+
+l3=[2]
+print(len(l3))
+'''if l3 != []:
+   print("list is not empty")
+else:
+   print("list is empty")
+'''
+if len(l3) == 0:
+   print("list is  empty")
+else:
+   print("list is not empty")
+
+if not l3:
+   print("list is  empty")
+else:
+   print("list is not empty")
+   
+INTEGER =10.0
+FLOAT =10
+if type(INTEGER) == float:
+    INTEGER=int(INTEGER)
+if type(FLOAT) == int:
+    FLOAT=float(FLOAT)
+print(INTEGER)
+print(FLOAT)    
+      
+units = float(input("Enter the total units of electricity consumed: "))
+# Initialize the bill amount
+bill_amount = 0
+# Calculate the bill based on the given criteria
+if units > 300:
+    bill_amount += (units - 300) * 5
+    units = 300
+if units > 100:
+    bill_amount += (units - 100) * 2
+# Display the calculated bill amount
+print(f"The total electric bill is: {bill_amount:.2f} Rs")
+
 
---
+++ loops_statements.py
@@ -0,0 +1,73 @@
+#for loop
+#n1=input("enter name 1:")
+'''l1=[]
+for i in range(5):
+    #print(i)
+    name=input("enter"+ " "+ str(i+1)+" "+"name:")
+    l1.append(name)
+
+print(l1)
+'''
+#Write a Python program to print numbers from 1 to 10 using a for loop.
+'''
+for i in range(10):
+    print(i+1)'''
+    
+#Write a Python program to calculate the sum of the first 20 natural numbers using a for loop.
+'''sum_nat=0
+for i in range(6):
+    print("initially",sum_nat)
+    print("my current i value:",i)
+    sum_nat=sum_nat+i
+    print("after adding my i value",sum_nat)
+print("my final output is",sum_nat)'''
+
+#Given a list fruits = ['apple', 'banana', 'cherry', 'date'], write a Python program to print each fruit in the list using a for loop.
+'''
+fruits = ['apple', 'banana', 'cherry', 'date']
+for name in fruits:
+    print(name) '''
+
+#Write a Python program to print the multiplication table of 5 using a for loop.
+'''
+for i in range(1,6):
+    print(str(i) + "*" + "5 =",i*5)'''
+
+#Write a Python program that takes a list of numbers and prints only the even numbers using a for loop.
+'''
+list_1=[4,5,7,8,9,10,33,22,55,66]
+list_2=[]
+list_3=[]
+for num in list_1:
+    if num %2 ==0:
+        list_2.append(num)
+    else:
+        list_3.append(num)
+print(list_1)
+print("even:",list_2)
+print("odd:",list_3)
+'''
+
+#Write a Python program to calculate the sum of all elements in a list numbers = [3, 5, 7, 9, 11] using a for loop.
+'''numbers = [3, 5, 7, 9, 11]
+b=0
+for ramya in numbers:
+    b=b+ramya
+print(b)'''
+
+#Write a Python program to count the number of times the character 'a' appears in the string "banana" using a for loop.
+'''string_d="banana"
+val_a=0
+for r in string_d:
+    print(r)
+    if r == 'a':
+        val_a=val_a+1
+print(val_a)
+'''
+
+#Write a Python program to reverse a given string using a for loop. Example: Input: "Python", Output: "nohtyP".
+d="python"
+word=""
+for r in d[::-1]:
+    word=word+r
+print(word)
